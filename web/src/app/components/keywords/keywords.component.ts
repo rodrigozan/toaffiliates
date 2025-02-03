687a2d86b2bc5f261554d8f54a11a7a1281b78cb
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-02-03 09:26:19 -0300

    Created keywords components

---
+++ web/src/app/components/keywords/keywords.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keywords',
+  standalone: true,
+  imports: [],
+  templateUrl: './keywords.component.html',
+  styleUrl: './keywords.component.scss'
+})
+export class KeywordsComponent {
+
+}
