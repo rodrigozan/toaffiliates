d2a5eef4f6849a29cc823c2e90b37d4321645f25
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-02-03 09:25:56 -0300

    Created product component

---
+++ web/src/app/components/product-data/product-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-data.component.html',
+  styleUrl: './product-data.component.scss'
+})
+export class ProductDataComponent {
+
+}
